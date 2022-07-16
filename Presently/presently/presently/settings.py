2152407a29d4d4521f20c656d2eebf4b79a81ce4
NaweenTharuka <61498921+NaweenTharuka@users.noreply.github.com>
2022-07-16 19:07:01 +0530

    updated

--- Presently/presently/presently/settings.py
+++ Presently/presently/presently/settings.py
@@ -137,6 +137,6 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/users/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+MEDIA_ROOT = os.path.join(BASE_DIR,"users/")
